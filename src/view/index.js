b054bb1359c8cedd3b8256b5f3d5c50cd7974890
xin-sui <2505258205@qq.com>
2023-11-16 17:58:58 +0800

    up

--- src/view/index.js
+++ src/view/index.js
@@ -1,12 +1,14 @@
 // //组件入口
 import * as components from '../components/index'
-import '../assets/base.css'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 export const install = {
   install: (app) => {
     for (const componentName in components) {
       const component = components[componentName]
       app.component(component.name, component)
     }
+    app.use(Antd)
   }
 }
 
